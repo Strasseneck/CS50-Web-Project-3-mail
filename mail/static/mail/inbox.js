90c2e09804fc841dd44f22a1b79c7df6d0aad0a4
Strasseneck <121754696+Strasseneck@users.noreply.github.com>
2023-07-25 11:09:12 +0200

    Add send mail functionality to compose_email function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -12,6 +12,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function compose_email() {
 
+  // Declare variables for use later
+  const submit = document.querySelector('#submit');
+  const mailRecipients = document.querySelector('#compose-recipients');
+  const mailSubject = document.querySelector('#compose-subject');
+  const mailBody = document.querySelector('#compose-body');
+
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -20,6 +26,32 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  // Listen for submission of form
+  document.querySelector('#compose-form').onsubmit = () => {
+    
+    // Get email content
+    const recipients = mailRecipients.value;
+    const subject = mailSubject.value;
+    const body = mailBody.value;
+    
+    // Send mail
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      // Print result
+      console.log(result);
+    });
+  }
+  // Load user's sent mailbox
+  load_mailbox('sent');
 }
 
 function load_mailbox(mailbox) {
@@ -31,3 +63,4 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
+
